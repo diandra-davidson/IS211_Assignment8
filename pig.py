719fbf5e8479671edeb4571ce90afebb75001e4e
d-vega <15893653+d-vega@users.noreply.github.com>
2019-03-24 23:12:28 -0400

    Computer logic 75% complete. Does not hold when intended at the moment. Factory class & TimeGameProxy class missing

--- pig.py
+++ pig.py
@@ -6,15 +6,17 @@
 import sys
 import random
 import argparse
+import os
+import time
 
 
-#PARSER = argparse.ArgumentParser()
-#PARSER.add_argument('--player1', help='Choose player type of player one.'
-#                                      ' [Human | Computer]')
-#PARSER.add_argument('--player2', help='Choose player type of player two.'
-#                                      ' [Human | Computer]')
-#PARSER.add_argument('--timed', help='How long the game will run in seconds.')
-#ARGS = PARSER.parse_args()
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('--player1', help='Choose player type of player one.'
+                                      ' [Human | Computer]')
+PARSER.add_argument('--player2', help='Choose player type of player two.'
+                                      ' [Human | Computer]')
+PARSER.add_argument('--timed', help='How long the game will run in seconds.')
+ARGS = PARSER.parse_args()
 
 
 class Player(object):
@@ -71,12 +73,14 @@ class Game(object):
         if choice is roll:
             diceroll = random.choice(dice)
             player.totalScore(diceroll)
-            print "You rolled a {}. Your total score is {}." \
-                  "\n".format(diceroll, player.score)
 
             if diceroll == 1:
                 print "You rolled a {}. Your turn now ends. Your total score" \
                       " is {}.\n".format(diceroll, player.score)
+            else:
+                print "You rolled a {}. Your total score is {}." \
+                      "\n".format(diceroll, player.score)
+
             return diceroll
 
         elif choice is hold:
@@ -96,7 +100,7 @@ class ComputerPlayer(Player):
         Player.__init__(self, score)
         self.turntotal = turntotal
 
-    def makeChoice(self, dice):
+    def makeChoice(self, dice=0):
         """Computer makes a choice using it's own logic."""
         self.turntotal += dice
         computer_score = self.score
@@ -104,6 +108,7 @@ class ComputerPlayer(Player):
         if dice == 1:
             turn_ends = True
             return turn_ends
+
         elif 100 - computer_score >= 25:
             if self.turntotal < 25:
                 choice = 'r'
@@ -112,7 +117,14 @@ class ComputerPlayer(Player):
             elif self.turntotal >= 25:
                 choice = 'h'
                 turn_ends = True
-            return choice, turn_ends
+
+        elif 100 - computer_score < 25:
+            new_hold = 100 - computer_score
+            if self.turntotal < new_hold:
+                choice = 'r'
+                turn_ends = False
+
+        return choice, turn_ends
 
 
 class PlayerFactory(object):
@@ -130,62 +142,138 @@ class TimedGameProxy(Game):
         return
 
 
-def playGame():
+def playGame(p_one, p_two):
     """Run the game"""
     playgame = Game()
-    playerone = Player()
-    playertwo = Player()
-    start_plyr_one = "Player 1's turn begins:" + "\n"
-    start_plyr_two = "Player 2's turn begins:" + "\n"
+
+    if p_one.lower() == 'human':
+        p_one_name = "Player 1"
+        playerone = Player()
+    elif p_one.lower() == 'computer':
+        p_one_name = "Computer Player 1"
+        playerone = ComputerPlayer()
+
+    if p_two.lower() == 'human':
+        p_two_name = "Player 2"
+        playertwo = Player()
+    elif p_two.lower() == 'computer':
+        p_two_name = "Computer Player 2"
+        playertwo = ComputerPlayer()
+
+
+    start_plyr_one = "{} 's turn: \n".format(p_one_name)
+    start_plyr_two = "{} 's turn: \n".format(p_two_name)
 
     while True:
         while playgame.status(playerone.score, playertwo.score) == False:
             print "\n" + start_plyr_one
-            choice = raw_input("Do you want to roll or hold? [r|h] ")
-            plyr_one_turn = playerone.playerChoice(choice)
+            if p_one == 'human':
+                choice = raw_input("Do you want to roll or hold? [r|h] ")
+                plyr_one_turn = playerone.playerChoice(choice)
 
-            if plyr_one_turn[1] == False:
-                if playgame.status(playerone.score, playertwo.score) == True:
-                    print playgame.status(playerone.score, playertwo.score)
-                    break
-                else:
-                    roll = playgame.rollDice(playerone, plyr_one_turn[0])
-                    if roll == (1, True):
+                if plyr_one_turn[1] == False:
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
                         break
                     else:
+                        roll = playgame.rollDice(playerone, plyr_one_turn[0])
+                        if roll == 1:
+                            time.sleep(3)
+                            break
+                        else:
+                            time.sleep(1)
+                            continue
+                elif plyr_one_turn == ('h', True):
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    else:
+                        playgame.rollDice(playerone, plyr_one_turn[0])
+                        time.sleep(1)
+                        break
+            elif p_one == 'computer':
+                choice = playerone.makeChoice()
+
+                if choice == ('r', False):
+                    print "{} rolls!".format(p_one_name)
+                    plyr_one_turn = playgame.rollDice(playerone, choice[0])
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    elif plyr_one_turn == 1:
+                        time.sleep(3)
+                        break
+                    else:
+                        time.sleep(2)
                         continue
-            elif plyr_one_turn == ('h', True):
-                if playgame.status(playerone.score, playertwo.score) == True:
-                    print playgame.status(playerone.score, playertwo.score)
-                    break
-                else:
-                    break
+                elif choice == ('h', True):
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    else:
+                        print "{} decided to hold. Turn ends.".format(p_one_name)
+                        playgame.rollDice(playerone, choice[0])
+                        time.sleep(2)
+                        break
 
         while playgame.status(playerone.score, playertwo.score) == False:
             print "\n" + start_plyr_two
-            choice = raw_input("Do you want to roll or hold? [r|h] ")
-            plyr_two_turn = playerone.playerChoice(choice)
+            if p_two == 'human':
+                choice = raw_input("Do you want to roll or hold? [r|h] ")
+                plyr_two_turn = playertwo.playerChoice(choice)
 
-            if plyr_two_turn[1] == False:
-                if playgame.status(playerone.score, playertwo.score) == True:
-                    print playgame.status(playerone.score, playertwo.score)
-                    break
-                else:
-                    roll = playgame.rollDice(playertwo, plyr_two_turn[0])
-                    if roll == (1, True):
+                if plyr_two_turn[1] == False:
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
                         break
                     else:
+                        roll = playgame.rollDice(playertwo, plyr_two_turn[0])
+                        if roll == (1, True):
+                            time.sleep(3)
+                            break
+                        else:
+                            continue
+                elif plyr_two_turn == ('h', True):
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    else:
+                        break
+            elif p_two == 'computer':
+                choice = playertwo.makeChoice()
+
+                if choice == ('r', False):
+                    print "{} rolls!".format(p_two_name)
+                    plyr_two_turn = playgame.rollDice(playertwo, choice[0])
+                    if playgame.status(playerone.score,
+                                       playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    elif plyr_two_turn == 1:
+                        time.sleep(3)
+                        break
+                    else:
+                        time.sleep(2)
                         continue
-            elif plyr_two_turn == ('h', True):
-                if playgame.status(playerone.score, playertwo.score) == True:
-                    print playgame.status(playerone.score, playertwo.score)
-                    break
+                elif choice == ('h', True):
+                    if playgame.status(playerone.score, playertwo.score) == True:
+                        print playgame.status(playerone.score, playertwo.score)
+                        break
+                    else:
+                        print "{} decided to hold. Turn ends.".format(p_two_name)
+                        playgame.rollDice(playertwo, choice[0])
+                        time.sleep(2)
+                        break
                 else:
-                    break
-        continue
+                    print "Something happened!"
     return
 
 
 if __name__ == '__main__':
     """Run the game"""
-    print playGame()
+    if ARGS.player1 and ARGS.player2:
+        print playGame(ARGS.player1, ARGS.player2)
+    else:
+        print "Invalid arguments specified. Please see python pig.py -h for " \
+              "help."
+        os.system('python pig.py -h')
